371f9f7cc42af1215e1f2b3068f050581b490dd6
muzagov <muzagov95@mail.ru>
2020-05-02 17:28:43 +0300

    shop

---
+++ shop/precache-manifest.b83586bf3d96a1cceb1cd3b367840611.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d331b83984c3d83fb75f6a2752913e76",
+    "url": "./index.html"
+  },
+  {
+    "revision": "775221aad9f678dfd78f",
+    "url": "./static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "775221aad9f678dfd78f",
+    "url": "./static/js/2.d12194cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d12194cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "544781f722553cbfad1e",
+    "url": "./static/js/main.0d3132f7.chunk.js"
+  },
+  {
+    "revision": "13c3b963b6a67e501cb6",
+    "url": "./static/js/runtime-main.9c01849e.js"
+  },
+  {
+    "revision": "4e6c5b599de59e4850083a1aeb081a66",
+    "url": "./static/media/full-word.4e6c5b59.svg"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "./static/media/shopping-cart-solid.8bf1af37.svg"
+  }
+]);
