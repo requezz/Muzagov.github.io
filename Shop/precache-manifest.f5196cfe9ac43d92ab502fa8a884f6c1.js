224ffe9efa8a907c84e43f95aee9829c5182407a
muzagov <muzagov95@mail.ru>
2020-05-02 16:11:33 +0300

    add shop

---
+++ Shop/precache-manifest.f5196cfe9ac43d92ab502fa8a884f6c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3ad654cc62d2d80709d594f54aa3ac4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "775221aad9f678dfd78f",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "775221aad9f678dfd78f",
+    "url": "/static/js/2.d12194cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d12194cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "544781f722553cbfad1e",
+    "url": "/static/js/main.0d3132f7.chunk.js"
+  },
+  {
+    "revision": "79c385ac60138402e804",
+    "url": "/static/js/runtime-main.e16a4cdb.js"
+  },
+  {
+    "revision": "4e6c5b599de59e4850083a1aeb081a66",
+    "url": "/static/media/full-word.4e6c5b59.svg"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/static/media/shopping-cart-solid.8bf1af37.svg"
+  }
+]);
