f4b969eb6d9599483a5882069b5e58ea6a4c14c6
muzagov <muzagov95@mail.ru>
2020-05-03 03:15:39 +0300

    add counter

---
+++ counter/precache-manifest.29ab0e39299c3c8591c4bccb1b0ddfc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54e638cd1d27128620128ee4ac45f700",
+    "url": "./index.html"
+  },
+  {
+    "revision": "edce7a2c08f49ebf3c67",
+    "url": "./static/js/2.f417ccaa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.f417ccaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d66c3c7c5d79add6712",
+    "url": "./static/js/main.c21ecfae.chunk.js"
+  },
+  {
+    "revision": "a126dcc470197a062998",
+    "url": "./static/js/runtime-main.bb193d6c.js"
+  }
+]);
