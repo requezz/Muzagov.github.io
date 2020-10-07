969a186a56843d0fa801bb245c36eeb62ca40790
muzagov <muzagov95@mail.ru>
2020-10-07 22:06:40 +0300

    add build

---
+++ build/precache-manifest.c85085a9c8801c39a35291080664fd49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14999d79350ce8e0a0d812ecd93979cc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "45890fb5f65cb8ac606d",
+    "url": "./static/css/main.a4d31da6.chunk.css"
+  },
+  {
+    "revision": "d37d98349e02c5813170",
+    "url": "./static/js/2.2ad6044b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.2ad6044b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45890fb5f65cb8ac606d",
+    "url": "./static/js/main.999cce7f.chunk.js"
+  },
+  {
+    "revision": "b7ad14b6b0f3382a48e9",
+    "url": "./static/js/runtime-main.a4190f94.js"
+  }
+]);
